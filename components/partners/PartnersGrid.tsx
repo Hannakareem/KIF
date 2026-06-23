62fda4345f2ca2c7c378657409700b686e559f70
nehlaaaah <nehlafathimah246@gmail.com>
2026-06-23 15:02:44 +0530

    Add partners section and lanyard integration

--- components/partners/PartnersGrid.tsx
+++ components/partners/PartnersGrid.tsx
@@ -1,6 +1,10 @@
-import Partners from "./Partners";
-import type { PartnerLogo } from "../../lib/types";
+import PartnersGrid from "../../components/partners/PartnersGrid";
+import partners from "../../data/partners.json";
 
-export default function PartnersGrid({ logos }: { logos?: PartnerLogo[] }) {
-  return <Partners logos={logos} />;
+export default function PartnersPage() {
+  return (
+    <main className="min-h-screen bg-[var(--color-bg)] text-[var(--color-foreground)]">
+      <PartnersGrid logos={partners} />
+    </main>
+  );
 }
